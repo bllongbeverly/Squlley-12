3f4716d8b2c46b1c9e20ea78d1e9c1bcd93462d8
bllongbeverly <bllongbeverly@gmail.com>
2023-06-18 18:51:37 -0500

    new files, .gitignore, schema.sql

---
+++ schema.sql
@@ -0,0 +1,14 @@
+department
+id: INT PRIMARY KEY
+name : VARCHAR(30) 
+role 
+id : INT PRIMARY KEY
+title : VARCHAR (30)
+salary : DECIMAL
+department_id : INT
+employee
+id : INT PRIMARY KEY
+first name : VARCHAR (30)
+last name : VARCHAR (30)
+role_id : INT
+manager_id : INT
